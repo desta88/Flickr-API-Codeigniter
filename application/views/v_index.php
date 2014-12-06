e4e6a68d3e3076484f50d85361c95bad6189ba6d
Andhika Desta Permana <mdcreative.indonesia@gmail.com>
2014-12-07 02:38:13 +0700

    Change Framewrok CI
    Deleting default CI settings

---
+++ application/views/v_index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>phpFlickr in CodeIgniter</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Example API</h1>
+</div>
+
+</body>
+</html>
